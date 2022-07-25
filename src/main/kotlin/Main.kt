3a45fd23a1bf88a461839d8341448062b3bfe2fc
Douglas <douglasonkeo3@gmail.com>
2022-07-25 20:44:50 +0300

    string templates in Kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,9 +3,17 @@ fun main() {
     var q  = Question()
     q.answer = "34"
 //    q.name = "James"
+    q.display()
+    // use ${} for compound variables
+    println("You said your answer was ${q.answer}")
 
 }
 class Question {
     var answer = ""
     val name = "Douglas"
+
+    fun display() {
+        // $ only for single variables
+        println("You said $answer")
+    }
 }
