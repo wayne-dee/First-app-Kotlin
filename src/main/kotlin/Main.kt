365651af3f2ad04604a4f8e55c0b95e57af535fe
Douglas <douglasonkeo3@gmail.com>
2022-07-25 20:54:42 +0300

    if statements

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,9 +7,17 @@ fun main() {
     // use ${} for compound variables
     println("You said your answer was ${q.answer}")
 
+    // if statements OR we can use the other syntax
+    val message = if (q.answer == q.correct) {
+        "You are correct"
+    }else {
+        "Try again"
+    }
+    println(message)
 }
 class Question {
     var answer = ""
+    var correct = "45"
     val name = "Douglas"
 
     fun display() {
